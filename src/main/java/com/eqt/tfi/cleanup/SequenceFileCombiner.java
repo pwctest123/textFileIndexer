12ac5d6d928d76c8671b4188486ddeccdaa05af7
gbarton <garrett.barton@gmail.com>
2013-10-23 21:14:15 -0400

    modded combiner to take a override for the number of files to create.

--- src/main/java/com/eqt/tfi/cleanup/SequenceFileCombiner.java
+++ src/main/java/com/eqt/tfi/cleanup/SequenceFileCombiner.java
@@ -3,6 +3,7 @@ package com.eqt.tfi.cleanup;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
